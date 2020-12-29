d04c7c3006ad9374123f32d430d02ca651b5812a
Adingame <45685592+Adingame@users.noreply.github.com>
2020-12-29 17:03:31 +0600

    Temlplate method

---
+++ HomeWork/Builder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork
+{
+
+    public class Toyota : CarBase
+    {
+        public override void AddBox()
+        {
+            Console.WriteLine("Best box");
+        }
+
+        public override void AddCarcase()
+        {
+            Console.WriteLine("Best Carcase");
+        }
+
+        public override void AddMotor()
+        {
+            Console.WriteLine("Best motor");
+        }
+
+        public override void AddWheel()
+        {
+            Console.WriteLine("Best wheel");
+        }
+    }
+
+    public abstract class CarBase
+    {
+        public string carName { get; set; }
+        public void CreateCar(string CarName)
+        {
+            carName = CarName;
+            AddBox();
+            AddWheel();
+            AddMotor();
+            AddCarcase();
+        }
+
+        public abstract void AddBox();
+        public abstract void AddWheel();
+        public abstract void AddMotor();
+        public abstract void AddCarcase();
+    }
+}
---
+++ HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main()
+        {
+            CarBase car = new Toyota();
+            car.CreateCar("Toyota");
+        }
+    }
+}
